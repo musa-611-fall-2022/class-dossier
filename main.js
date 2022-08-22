aff292bbe134c660cac52752a0bf28d463ae8ace
Mjumbe Poe <mjumbewu@gmail.com>
2022-08-21 21:40:22 -0400

    Add test student file names to script

--- main.js
+++ main.js
@@ -3,13 +3,22 @@ const instructorNames = [
   'mjumbepoe',
   'jingyili',
 ];
-const studentNames = [];
+const studentNames = [
+  'makaylaharper',
+  'osmanbarron',
+  'mistake',
+  'tameraaustin',
+];
 
 // Object mapping person names to data loaded from JSON files.
 let people = {};
 
 async function loadPersonData(personName) {
   const response = await fetch(`data/${personName}.json`);
+  if (response.status >= 400) {
+    throw Error(`No person data for ${personName} found.`);
+  }
+
   const personData = await response.json();
   return personData;
 }
